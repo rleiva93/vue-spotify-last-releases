02bf933cbd6b26b20976442af2d8f58bd102ab88
Ricardo Leiva <ricardo.leivacastillo@gmail.com>
2020-09-23 20:58:29 -0600

    update github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <v-footer dark fixed class="font-weight-medium">
     <v-col class="text-center" cols="12">
       <a
-        href="https://github.com/rleiva93/spotify-webapp"
+        href="https://github.com/rleiva93/vue-spotify-last-releases"
         target="_blank"
         class="text-decoration-none"
       >
